503d776077508d543d6798cddc18b0b75ae2d8d8
Santiago Ruiz <santiago.ruiz@edu.uag.mx>
2016-10-05 14:46:57 -0500

    Visual Studio Project

---
+++ BinaryTrees_N_Hash/BinaryTrees_N_Hash/Node.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTrees_N_Hash
+{
+    class Node
+    {
+        /* Structs */
+        public struct UserInfo
+        {
+            string sName;
+            string sAddress;
+            string sPhone;
+        }
+
+        /* Data Members */
+        public  int      iID;
+        public  Node     pLeft;
+        public  Node     pRight;
+        private UserInfo stUserInfo;
+
+        /* Functions */
+        public Node()
+        {
+            Console.WriteLine("NODE Created");
+        }
+
+        ~Node()
+        {
+
+        }
+    }
+}
---
+++ BinaryTrees_N_Hash/BinaryTrees_N_Hash/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTrees_N_Hash
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Node cNode = new Node();
+            Console.WriteLine("HELLO WORLD");
+            Console.ReadKey();
+        }
+    }
+}
