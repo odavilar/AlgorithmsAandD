0ae5f163d26ddb7af4dc1f51c7032cde1b65ab5b
Santiago Ruiz <santiago.ruiz@edu.uag.mx>
2016-10-06 08:44:04 -0500

    Ignore

---
+++ ignore.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+git update-index --assume-unchanged BinaryTrees_N_Hash/BinaryTrees_N_Hash.sln
+git update-index --assume-unchanged BinaryTrees_N_Hash/BinaryTrees_N_Hash.v12.suo
+git update-index --assume-unchanged BinaryTrees_N_Hash/BinaryTrees_N_Hash/App.config
+git update-index --assume-unchanged BinaryTrees_N_Hash/BinaryTrees_N_Hash/BinaryTrees_N_Hash.csproj
+git update-index --assume-unchanged BinaryTrees_N_Hash/BinaryTrees_N_Hash/Properties/AssemblyInfo.cs
