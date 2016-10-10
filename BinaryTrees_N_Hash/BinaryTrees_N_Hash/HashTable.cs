43631d028807f83841fdb67c780a48d0ed6bb024
Oscar Davila <odavilar@gmail.com>
2016-10-10 07:59:31 -0500

    Hash working

--- BinaryTrees_N_Hash/BinaryTrees_N_Hash/HashTable.cs
+++ BinaryTrees_N_Hash/BinaryTrees_N_Hash/HashTable.cs
@@ -12,51 +12,26 @@ namespace BinaryTrees_N_Hash
         public const int u32DefaultSize = 5;
     }
 
-    static class HelperFuncs
+    public class HashTable
     {
-        public static bool HasMethod(this object objectToCheck, string methodName)
-        {
-            try
-            {
-                var type = objectToCheck.GetType();
-                return type.GetMethod(methodName) != null;
-            }
-            catch (AmbiguousMatchException)
-            {
-                return true;
-            }
-        }
+        public Node[] au32Array;
 
-        public static bool HasProperty(this object objectToCheck, string propertyName)
-        {
-            try
-            {
-                var type = objectToCheck.GetType();
-                return type.GetProperty(propertyName) != null;
-            }
-            catch (AmbiguousMatchException)
-            {
-                return true;
-            }
-        }
-    }
-
-    public class HashTable<T>
-    {
-        public T[] tArray;
         /* Constructor */
         public HashTable()
         {
-            tArray = new T[HTConst.u32DefaultSize];
-            if(tArray[0].GetType() != typeof(int))
-            {
-                if (!HelperFuncs.HasProperty(tArray[0], "id"))
-                {
-                    throw new ArgumentException("Object type not support; missing property id.");
-                }
-            }
+            au32Array = new Node[HTConst.u32DefaultSize];
+            Console.WriteLine("HashTable with size " + HTConst.u32DefaultSize + " has been created");
+        }
 
-            Console.WriteLine("HashTable with size " + HTConst.u32DefaultSize + "has been created");
+        public HashTable(UInt32 u32Size)
+        {
+            au32Array = new Node[u32Size];
+            Console.WriteLine("HashTable with size " + u32Size + "has been created");
+        }
+
+        private uint HashFunction(UInt32 u32ID)
+        {
+            return FNV1a.u32Compute(u32ID) % (UInt32)au32Array.Length;
         }
 
         /* Destructor */
@@ -65,21 +40,68 @@ namespace BinaryTrees_N_Hash
             Console.WriteLine("HashTable Destroyed");
         }
 
-        public int Add(T tObject)
+        public void Add(Node cNode)
         {
-            int i32Error = 0;
-
-            return i32Error;
+            UInt32 u32HashTableIndex = 0;
+            u32HashTableIndex = HashFunction(cNode.u32ID);
+            if (au32Array[u32HashTableIndex] == null)
+            {
+                au32Array[u32HashTableIndex] = cNode;
+                Console.WriteLine("Index Empty");
+            }
+            else
+            {
+                Console.WriteLine("Index Already Used");
+            }
         }
 
-        public int Delete(T tObject)
+        public void Delete(UInt32 u32ID)
         {
-            return 0;
+            UInt32 u32HashTableIndex = 0;
+
+            u32HashTableIndex = HashFunction(u32ID);
+
+            if (au32Array[u32HashTableIndex] == null)
+            {
+                Console.WriteLine("Not Found");
+            }
+            else
+            {
+                au32Array[u32HashTableIndex] = null;
+                Console.WriteLine("Deleted");
+            }
         }
 
-        public int Search(T tObject)
+        public Node Search(UInt32 u32ID)
         {
-            return 0;
+            UInt32 u32HashTableIndex = 0;
+            u32HashTableIndex = HashFunction(u32ID);
+            return au32Array[u32HashTableIndex];
+        }
+    }
+
+    static class FNV1a
+    {
+        /* http://www.isthe.com/chongo/tech/comp/fnv/index.html */
+        /* 32 bit FNV_prime = 224 + 28 + 0x93 = 16777619 */
+        /* 32 bit offset_basis = 2166136261 */
+
+        static public UInt32 u32Compute(UInt32 u32ID)
+        {
+            UInt32 u32Hash = 0;
+            byte[] au8Byte;
+            const UInt32 u32FNVprime = 16777619;
+            const UInt32 u32OffsetBasis = 2166136261;
+
+            au8Byte = BitConverter.GetBytes(u32ID);
+            u32Hash = u32OffsetBasis;
+            for (var i = 0; i < au8Byte.Length; i++)
+            {
+                u32Hash = u32Hash ^ au8Byte[i];
+                u32Hash = u32Hash * u32FNVprime;
+            }
+
+            return u32Hash;
         }
     }
 }
