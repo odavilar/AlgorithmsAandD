9a81adc85a47e834afd3032242ce88394d8257f1
Santiago Ruiz <santiago.ruiz@edu.uag.mx>
2016-10-10 08:00:52 -0500

    Remove Ignore Script

--- ignore.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-git update-index --assume-unchanged BinaryTrees_N_Hash/BinaryTrees_N_Hash.sln
-git update-index --assume-unchanged BinaryTrees_N_Hash/BinaryTrees_N_Hash.v12.suo
-git update-index --assume-unchanged BinaryTrees_N_Hash/BinaryTrees_N_Hash/App.config
-git update-index --assume-unchanged BinaryTrees_N_Hash/BinaryTrees_N_Hash/BinaryTrees_N_Hash.csproj
-git update-index --assume-unchanged BinaryTrees_N_Hash/BinaryTrees_N_Hash/Properties/AssemblyInfo.cs
